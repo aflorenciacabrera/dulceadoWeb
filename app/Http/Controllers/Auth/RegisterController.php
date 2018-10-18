57a4fe88a8f868b6a3f468744676c8664692368b
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-10-18 17:13:46 -0300

    roles

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,16 +63,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-         $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-
-         $user
+        $user = User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => bcrypt($data['password']),
+    ]);
+    $user
         ->roles()
         ->attach(Role::where('name', 'user')->first());
-        
     return $user;
     }
 
