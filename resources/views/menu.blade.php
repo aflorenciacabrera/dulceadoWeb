aacbe6000af42796c33ebea5d86fe15c2c10e983
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-10-18 21:33:38 -0300

    menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,39 +2,32 @@
 @section('content')
 
 <div class="container">
-    <div class="row justify-content-center">
-        
+        <div class="row justify-content-center">
+                <div class="links">
    {{-- Panel de botones --}}
-    
-        
-            <div class="links">
-                
-   {{-- Panel de botones --}}
-    
                     {{-- Pedidos --}}
-                    <a class="btn btn-info active btn-lg" role="button" href="{{ url('/admin/pedidos') }}">
+                    <a class="btn btn-info  btn-lg" role="button" href="{{ url('/admin/pedidos') }}">
                         <img src="{{asset('img/realiza_pedido.png')}}" width="100" /><br>
-                        <span class="label label-default">Pedido </span> </a>
-
-                        
+                        <span class="label label-default">Pedido </span> </a> 
                     {{-- Productos--}}
-                    <a class="btn btn-warning active btn-lg" role="button" href="{{ url('admin/producto/producto')}}">
+                     <a class="btn btn-warning  btn-lg" role="button" href="{{ url('admin/producto/producto') }}">
                         <img src="{{asset('img/easter-egg-bunny-pattern-icon-68527.png')}}" width="100" /><br>
-                    <span class="label label-default">Productos </span> </a>
-                    {{-- Control Stock --}}
-                    
-                    <a class="btn btn-danger active btn-lg" role="button"  href="{{ url('/admin/stock/stock') }}"> 
+                        <span class="label label-default">Pedido </span> </a>
+                    {{-- Control Stock --}}       
+                    <a class="btn btn-danger  btn-lg" role="button"  href="{{ url('/admin/stock/stock') }}"> 
                         <img src="{{asset('img/Control-de-stock.png')}}" width="100" /><br>
                         <span class="label label-default">Control Stock</span></a>
                     {{-- Clientes --}}
-                    <a class="btn btn-primary active btn-lg" role="button"  href="{{ url('/admin/cliente/cliente') }}">
+                    <a class="btn btn-primary  btn-lg" role="button"  href="{{ url('/admin/cliente/cliente') }}">
                          <img src="{{asset('img/los-diferentes-pc3bablicos-de-una-empresa.png')}}" width="100"/><br>
                          <span class="label label-default">Clientes </span></a>
                     {{-- Gastos --}}
-                    <a class="btn btn-success active btn-lg" role="button"  href="{{ url('/admin/gasto/gasto') }}"> 
+                    <a class="btn btn-success  btn-lg" role="button"  href="{{ url('/admin/gasto/gasto') }}"> 
                         <img src="{{asset('img/images.jpg')}}" width="100" /><br>
                         <span class="label label-default">Gastos </span></a>
  
     {{--Fin de Panel de Botones--}}
- </div> </div> </div> 
+                </div> 
+        </div> 
+</div> 
 @endsection
