d4ace48e6981ffbfb38a7dc8081ee7aef6a6dbd2
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-10-18 21:43:28 -0300

    menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -6,19 +6,19 @@
                 <div class="links">
    {{-- Panel de botones --}}
                     {{-- Pedidos --}}
-                    <a class="btn btn-info  btn-lg" role="button" href="{{ url('/admin/pedidos') }}">
+                    <a class="btn btn-info  btn-lg" role="button" href="{{ url('pedidos') }}">
                         <img src="{{asset('img/realiza_pedido.png')}}" width="100" /><br>
                         <span class="label label-default">Pedido </span> </a> 
                     {{-- Productos--}}
-                     <a class="btn btn-warning  btn-lg" role="button" href="{{ url('admin/producto/producto') }}">
+                     <a class="btn btn-warning  btn-lg" role="button" href="{{ url('producto') }}">
                         <img src="{{asset('img/easter-egg-bunny-pattern-icon-68527.png')}}" width="100" /><br>
                         <span class="label label-default">Pedido </span> </a>
                     {{-- Control Stock --}}       
-                    <a class="btn btn-danger  btn-lg" role="button"  href="{{ url('/admin/stock/stock') }}"> 
+                    <a class="btn btn-danger  btn-lg" role="button"  href="{{ url('stock') }}"> 
                         <img src="{{asset('img/Control-de-stock.png')}}" width="100" /><br>
                         <span class="label label-default">Control Stock</span></a>
                     {{-- Clientes --}}
-                    <a class="btn btn-primary  btn-lg" role="button"  href="{{ url('/admin/cliente/cliente') }}">
+                    <a class="btn btn-primary  btn-lg" role="button"  href="{{ url('cliente') }}">
                          <img src="{{asset('img/los-diferentes-pc3bablicos-de-una-empresa.png')}}" width="100"/><br>
                          <span class="label label-default">Clientes </span></a>
                     {{-- Gastos --}}
