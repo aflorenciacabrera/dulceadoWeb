b99397c50d4635a3fe132c3f319e6742b16397fc
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-10-23 00:33:59 -0300

    pagination

--- resources/views/producto/producto.blade.php
+++ resources/views/producto/producto.blade.php
@@ -64,6 +64,19 @@
                         </tbody>
                 </table>
 {{-- ***********************Pagination********************************* --}}
+            <div class="col-lg-3 offset-lg-4">
+                <nav >
+                    <ul class="pagination  ">
+                        <li class="page-item"><a class="page-link" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                        <li class="page-item"><a class="page-link">1</a></li>
+                        <li class="page-item"><a class="page-link">2</a></li>
+                        <li class="page-item"><a class="page-link">3</a></li>
+                        <li class="page-item"><a class="page-link">4</a></li>
+                        <li class="page-item"><a class="page-link">5</a></li>
+                        <li class="page-item"><a class="page-link" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>          
         </div>
 	</div>
